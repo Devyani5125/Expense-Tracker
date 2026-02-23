ea5c2290e8c237f34c1f50a6386eb8268373f8d6
cyber stream <cyber.stream151205@gmail.com>
2026-02-23 17:10:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-264589508-6dda9",
+  "appId": "1:470749789542:web:b2ee2ebc943380cef1dca7",
+  "apiKey": "AIzaSyDLDzoNM8vmCi3hYQdHBlWTxtCgrb0uCtU",
+  "authDomain": "studio-264589508-6dda9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "470749789542"
+};
