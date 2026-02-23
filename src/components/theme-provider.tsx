188e0c786615a276ef1cf1cb9b78a5088ecc60d6
cyber stream <cyber.stream151205@gmail.com>
2026-02-23 17:03:39 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'ne

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,7 +1,20 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+type ThemeProviderProps = {
+  children: React.ReactNode
+  attribute?: string | 'class'
+  defaultTheme?: string
+  enableSystem?: boolean
+  storageKey?: string
+  forcedTheme?: string
+  disableTransitionOnChange?: boolean
+  enableColorScheme?: boolean
+  value?: { [key: string]: string }
+  nonce?: string
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
